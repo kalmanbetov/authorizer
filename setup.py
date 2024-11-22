5453b920317237c52f1df881006a16f5986db4a8
Aman Kalmanbetov <aman@mancho.dev>
2024-11-22 15:19:22 +0600

    version 5

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="authorizer",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     install_requires=["pycryptodome"],
-    author="Andy",
-    author_email="andy@dont_spam_me.co",
-    description="A simple example private package",
+    author="Aman Kalmanbetov",
+    author_email="kalmanbetovaman@gmail.com",
+    description="Authorizer for Payments Gateway server and Payments Gateway clients",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/nydasco/package_publishing",
+    url="https://@github.com/kalmanbetov/authorizer",
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.6',
 )
