be89724d3faf9dbf5b08511fc9a83bdf084b3899
Aman Kalmanbetov <aman@mancho.dev>
2024-11-22 12:40:01 +0600

    version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="authorizer",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=["pycryptodome"],
     author="Andy",
