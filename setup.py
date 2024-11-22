8ccbcce7155ca19f60cc984f19300fe22e8551eb
Aman Kalmanbetov <aman@mancho.dev>
2024-11-22 15:11:08 +0600

    Added license to project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="authorizer",
-    version="0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=["pycryptodome"],
     author="Andy",
